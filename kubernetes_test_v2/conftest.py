a15b41ee71e189300710ca66c39ee942fa4a4090
ben_1 <ben@streamlinedanalytics.co.uk>
2025-11-10 21:46:25 +0000

    Initial commit

---
+++ kubernetes_test_v2/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from kubernetes_test_v2.users.models import User
+from kubernetes_test_v2.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def _media_storage(settings, tmpdir) -> None:
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
